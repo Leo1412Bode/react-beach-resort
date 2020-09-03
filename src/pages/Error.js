8fefb4261193eb2cbb9907dab493d434e36095fd
unknown <Leonardo>
2020-09-03 13:27:36 -0400

    leo

---
+++ src/pages/Error.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Error() {
+  return <div>Hello from Error Page</div>;
+}
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Error extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Error(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Error.propTypes = {
+
+}
+
+export default Error
+
+
